94195e7dd83bc444ff3bef8c0367d724d920e54b
Anupam Krishnamurthy <anupam.krishnamurthy@bitgrip.de>
2025-03-18 06:10:38 +0100

    Implement conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
